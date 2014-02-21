8ba97118d7b9facd1f546d9d23d064218a6e66ae
Kristofer Karlsson <krka@spotify.com>
2014-02-21 13:47:37 +0100

    Use JMH for benchmarking performance

---
+++ benchmark/run.sh
@@ -0,0 +1,3 @@
+mvn clean package
+java -jar target/microbenchmark.jar
+
---
+++ benchmark/src/main/java/com/spotify/sparkey/system/AppendBenchmark.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright (c) 2011-2014 Spotify AB
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.spotify.sparkey.system;
+
+import com.spotify.sparkey.CompressionType;
+import com.spotify.sparkey.Sparkey;
+import com.spotify.sparkey.SparkeyReader;
+import com.spotify.sparkey.SparkeyWriter;
+import org.openjdk.jmh.annotations.*;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+@State(Scope.Benchmark)
+@Warmup(iterations = 4)
+@Measurement(iterations = 10)
+public class AppendBenchmark {
+
+  private File indexFile;
+  private File logFile;
+  private SparkeyWriter writer;
+
+  @Setup(Level.Trial)
+  public void setup() throws IOException {
+    indexFile = new File("test.spi");
+    logFile = Sparkey.getLogFile(indexFile);
+
+    CompressionType compressionType = CompressionType.valueOf(type);
+
+    indexFile.deleteOnExit();
+    logFile.deleteOnExit();
+    indexFile.delete();
+    logFile.delete();
+
+    writer = Sparkey.createNew(indexFile, compressionType, 1024);
+  }
+
+  @TearDown(Level.Trial)
+  public void tearDown() throws IOException {
+    writer.close();
+    indexFile.delete();
+    logFile.delete();
+  }
+
+  @Param({"NONE", "SNAPPY"})
+  public String type;
+
+  @GenerateMicroBenchmark
+  @BenchmarkMode(Mode.Throughput)
+  @OutputTimeUnit(TimeUnit.SECONDS)
+  public void testSmall() throws IOException {
+    writer.put("key" , "value");
+  }
+
+  private static final String MEDIUM_KEY = String.format("%200s", "key");
+  private static final String MEDIUM_VALUE = String.format("%200s", "value");
+
+  @GenerateMicroBenchmark
+  @BenchmarkMode(Mode.Throughput)
+  @OutputTimeUnit(TimeUnit.SECONDS)
+  public void testMedium() throws IOException {
+    writer.put(MEDIUM_KEY , MEDIUM_VALUE);
+  }
+
+}
